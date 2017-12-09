c6da243a35a5836360adce3077a79ea32e399acd
IntPtr <lxy.lixiaoyan@gmail.com>
2017-12-09 21:46:24 +0800

    add nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import "./App.css";
 
 const App = () => (
   <div className="App">
+    <nav className="App-nav">
+      <a
+        href="https://github.com/react-moe"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+      <a
+        href="https://twitter.com/ReactMoe"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+    </nav>
     <img
       className="App-placeholder"
       src={placeholder}
